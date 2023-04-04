d532bc7be233f669cdf90c551ff685c663230403
Maverick Reynolds <mcreynolds02@gmail.com>
2023-04-04 00:55:23 -0400

    Refactoring string_adjustments

---
+++ string_adjustments.py
@@ -0,0 +1,107 @@
+'''
+Functions to adjust the title, location, and other fields for front/back end
+
+Maverick Reynolds
+UCF Crimes
+
+'''
+
+import re
+import editdistance
+import json
+
+TYPO_TOLERANCE = 1
+
+
+# Sometimes spacing isn't quite correct, or words spelled differently
+def case_title_format(case_title: str) -> str:
+    case_title = case_title.replace('PETIT', 'PETTY')
+    case_title = re.sub(' ?, ?', ', ', case_title) # Ensure commas have spaces following
+    case_title = re.sub('  ', ' ', case_title) # Remove double spaces
+    return case_title
+
+
+# Because emojis are fun
+# Reads from emojis.json (supports regex!)
+def attach_emojis(case_title: str) -> str:
+    with open('emojis.json', 'r', encoding="utf-8") as f:
+        emojis: dict = json.load(f)
+
+    emoji_suffix = ''
+    for emoji_txt in emojis.keys():
+        if re.search(emoji_txt, case_title.lower()):
+            emoji_suffix += f' {emojis[emoji_txt]}' # Must use += and not .join() to preserve unicode
+
+    return case_title + emoji_suffix
+
+
+# Returns title function applied to string with exceptions found in title_exceptions.json
+def gen_title(str_upper: str) -> str:
+    with open('title_exceptions.json') as f:
+        exceptions: dict = json.load(f)
+
+    title = ''
+    for token in str_upper.split():
+        if token in exceptions.keys():
+            title += exceptions[token]
+        else:
+            title += token.title()
+        title += ' '
+
+    return title.strip()
+
+
+# Takes raw address and primes it for comparison
+def expand_address(address: str) -> str:
+    address = ' ' + address + ' '
+    address = re.sub('\.', '', address)   # Remove periods
+
+    subs = {
+        ' W ': ' WEST ',
+        ' E ': ' EAST ',
+        ' N ': ' NORTH ',
+        ' S ': ' SOUTH ',
+        'BLD' : 'BLVD',
+        ' ?(?: and |\/) ?': ' & '     # Change intersection of streets to &
+    }
+    for key, value in subs.items():
+        address = re.sub(key, value, address, flags=re.IGNORECASE)
+
+    return address.strip()
+
+
+# Takes address and compares it to the locations.json file
+# Robust against varying word positions and typo errors
+# Returns gen_title() of address if no match is found
+def replace_address(address: str) -> str:
+    txt_tokens = expand_address(address).split()
+
+    with open('locations.json') as f:
+        locs: dict[str] = json.load(f)
+
+    # Do this with every key
+    for key in locs.keys():
+        key_tokens = key.split()
+        is_match = True
+
+        # If numerical start, make sure it matches exactly
+        if re.match('\d+', txt_tokens[0]) and re.match('\d+', key_tokens[0]) and txt_tokens[0] != key_tokens[0]:
+            is_match = False
+            continue
+
+        # Go through each token in key
+        for key_token in key_tokens:
+            token_distances = [editdistance.eval(key_token, txt_token) for txt_token in txt_tokens]
+            # Fail if no token is within tokerance for all key tokens
+            if min(token_distances) > TYPO_TOLERANCE:
+                is_match = False
+                break
+
+        # If all tokenswithin tolerance, locations is found
+        if is_match:
+            return locs[key]
+    
+    # Otherwise return original
+    return gen_title(address)
+
+
