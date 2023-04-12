0273d1d434d686231f5b73c934cfdee63c61c2fd
Ethan Frakes <edfrakes@gmail.com>
2023-04-12 02:44:32 -0400

    Removed DataFrame from Heatmap

--- heatmap.py
+++ heatmap.py
@@ -1,5 +1,4 @@
 import json
-import pandas as pd
 import googlemaps
 import folium
 from folium.plugins import HeatMap
@@ -11,8 +10,8 @@ async def send_heatmap(message, API_key: str) -> None:
     with open('crimes.json', 'r') as f:
         crimes = json.load(f)
 
-    locations = pd.DataFrame(columns=["Lat", "Lon"])
     gmaps_key = googlemaps.Client(key=API_key)
+    m = folium.Map(location=[28.6, -81.2], zoom_start=15)
 
     for key, crime in crimes.items():
         address = f'{crime["Location"].replace("/", "")} Orlando FL, US.'
@@ -21,13 +20,7 @@ async def send_heatmap(message, API_key: str) -> None:
         lat = g[0]["geometry"]["location"]["lat"]
         long = g[0]["geometry"]["location"]["lng"]
 
-        locations = locations.append({'Lat':lat, 'Lon':long}, ignore_index=True)
-
-    m = folium.Map(location=[28.6, -81.2], zoom_start=15)
-    for i, row in locations.iterrows():
-        HeatMap([[row['Lat'], row['Lon'], 0.3]]).add_to(m)
-
-    print(locations)
+        HeatMap([[lat, long, 0.3]]).add_to(m)
 
     img_data = m._to_png(5)
     img = Image.open(io.BytesIO(img_data))
