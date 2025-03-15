9227c7103ff9a5fb07ac5cf5b5dcac951851a178
thefrostedfrakes <edfrakes@gmail.com>
2025-03-15 17:02:13 -0400

    Merge orlando.py and orange.py into one script

--- orlando_orange.py
+++ orlando_orange.py
@@ -1,9 +1,9 @@
 '''
 
-UCF Crimes: orlando.py
+UCF Crimes: orlando_orange.py
 
-An extension to UCF Crimes that adds all orlando active calls to separate database table, will
-be used for querying calls and displaying active call report each hour.
+An extension to UCF Crimes that adds all Orlando PD & OSCO active calls to separate database 
+tables, will be used for querying calls and displaying active call report each hour.
 
 Written by Ethan Frakes
 
@@ -15,7 +15,7 @@ from utils import setup_db
 import xmltodict
 import pandas as pd
 from time import sleep
-import traceback
+from datetime import datetime
 from configparser import ConfigParser
 from sqlalchemy import text
 from sqlalchemy.engine.base import Engine
@@ -91,26 +91,70 @@ def check_orlando_size_csv(csv_file: str) -> None:
 
     print(f"Current number of entries in orlando.csv: {csv_size}")
 
+def load_orange_active(engine: Engine) -> None:
+    '''
+    Load XML file from OCSO website with BeautifulSoup and parse the file to SQL table.
+    '''
+    connection = engine.connect()
+
+    r = requests.get('https://www.ocso.com/Portals/0/CFS_FEED/activecalls.xml')
+    soup = BeautifulSoup(r.content, "lxml-xml")
+    active_dict = xmltodict.parse(str(soup))
+
+    for crime in active_dict["CALLS"]["CALL"]:
+        try:
+            year_date = datetime.now().strftime("%Y-%j")
+            expanded_incident = f"{year_date}-{crime['@INCIDENT']}"
+            query = f"""SELECT * FROM orange_crimes WHERE incident = '{expanded_incident}'"""
+            result = connection.execute(text(query))
+            entrytime = datetime.strptime(crime["ENTRYTIME"], "%m/%d/%Y %I:%M:%S %p").strftime("%-m/%d/%Y %H:%M:%S")
+            if crime['LOCATION']:
+                location = crime['LOCATION'].replace("'", "''")
+            else:
+                location = crime['LOCATION']
+
+            if result.rowcount == 0:
+                query = f"""INSERT INTO orange_crimes (incident, entrytime, description, location, sector, zone, rd)""" \
+                        f"""VALUES ('{expanded_incident}', '{entrytime}', '{crime['DESC']}', '{location}', '{crime['SECTOR']}', '{crime["ZONE"]}', '{crime["RD"]}')"""
+
+            else:
+                query = f"""UPDATE orange_crimes SET entrytime = '{entrytime}', description = '{crime['DESC']}', """ \
+                        f"""location = '{location}', sector = '{crime['SECTOR']}', zone = '{crime["ZONE"]}', rd = '{crime["RD"]}' WHERE incident = '{expanded_incident}'"""
+                
+            connection.execute(text(query))
+            connection.commit()
+
+        except Exception as e:
+            print(f"Exception occurred: {type(e).__name__}: {str(e)}")
+            # traceback.print_exc()
+
+    query = f"SELECT count(*) AS exact_count FROM orange_crimes"
+    result = connection.execute(text(query)).fetchone()
+    print("Orange County crimes database updated.")
+    print(f"Current number of entries in database: {result[0]}")
+    connection.close()
+
 if __name__ == '__main__':
     main_config = ConfigParser()
     main_config.read('config.ini')
     engine = setup_db(main_config)
 
-    orlando_counter = 0
+    counter = 0
 
-    print("Starting Orlando time counter (every 10 mins)")
+    print("Starting Orlando PD & OCSO time counter (every 10 mins)")
     while True:
-        if orlando_counter >= 600:
-            orlando_counter = 0
+        if counter >= 600:
+            counter = 0
             try:
                 csv_file = "orlando.csv"
                 load_orlando_active_sql(engine)
                 load_orlando_active_csv(csv_file)
                 check_orlando_size_csv(csv_file)
+                load_orange_active(engine)
             except Exception as e:
                 print(f"Exception occurred: {type(e).__name__}: {str(e)}")
                 # traceback.print_exc()
 
-        orlando_counter += 1
+        counter += 1
 
         sleep(1)
