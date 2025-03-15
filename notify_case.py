c7dbd6a4cc0545397696d05bc7f8729525c4d304
thefrostedfrakes <edfrakes@gmail.com>
2025-03-15 16:53:23 -0400

    Remove old scrips/backup folder, merge send_telegram into notify_case

--- notify_case.py
+++ notify_case.py
@@ -1,12 +1,53 @@
 from configparser import ConfigParser
 from meta_toolkit import post_to_meta_both
 from datetime import date, datetime, timedelta
-from send_telegram import sendTeleg
+import telegram
 import utils
 from image import generate_image
 import pandas as pd
 from xed import XED
 from requests.exceptions import HTTPError
+
+def sendTeleg(message, config, photo=None):
+    sent = False
+    retry_c = 0
+    while sent == False:
+        try:
+            bot = telegram.Bot(token=config.get('TELEGRAM', 'BOT_TOKEN'))
+            if photo:
+                sent = bot.send_photo(chat_id=config.get('TELEGRAM', 'ROOM_ID'), photo=photo, caption=message, timeout=20, )
+            else:
+                sent = bot.send_message(chat_id=config.get('TELEGRAM', 'ROOM_ID'), text=message, timeout=20)
+        except Exception as err:
+            print('err.args:')
+            print(err.args)
+            print(f"Unexpected {err=}, {type(err)=}")
+            print("\nString err:\n"+str(err))
+            if retry_c > 4:
+                print('Telegram attempts exceeded. Message not sent.')
+                break
+            elif str(err) == 'Unauthorized':
+                print('Invalid Telegram bot token, message not sent.')
+                break
+            elif str(err) == 'Timed out':
+                retry_c += 1
+                print('Telegram timeout count: '+str(retry_c))
+                pass
+            elif str(err) == 'Chat not found':
+                print('Invalid Telegram Chat ID, message not sent.')
+                break
+            elif str(err)[:35] == '[Errno 2] No such file or directory':
+                print('Telegram module couldn\'t find an image to send.')
+                break
+            elif str(err) == 'Media_caption_too_long':
+                print('Telegram image caption lenght exceeds 1024 characters. Message not send.')
+                break
+            else:
+                print('[X] Unknown Telegram error. Message not sent.')
+                break
+        else:
+            print("Telegram message successfully sent.")
+    return sent
 
 def notify_crime(crime, main_config):
     # Reformat dates and times
